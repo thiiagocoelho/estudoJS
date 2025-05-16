6a0b1f956995db6bbfc9d586d1b105bc6f9bfb04
Thiago Henrique Coelho da Silva <JT0050348@mateus.dc>
2025-05-16 08:29:14 -0300

    Primeiro Comitt

---
+++ testes.js
@@ -0,0 +1,82 @@
+
+//Jogo de Adivinha numeros###############
+// let num = parseInt(Math.random() * 21);
+// let tentativas = 1;
+// let chute = Number(prompt("Adivinhe o número secreto entre 0 e 20:"));
+
+// while (true) {
+//     if (chute === num) {
+//         alert(`Acertou com ${tentativas} tentativa${tentativas === 1 ? '' : 's'}`);
+//         break;
+//     }
+    
+//     alert(`Número secreto é ${chute > num ? "menor" : "maior"} que ${chute}`);
+//     chute = Number(prompt("Tente novamente:"));
+//     tentativas++;
+// }
+
+//###################################################################################
+
+//Calculo de medias usando array##########
+// let notas = []
+// let tamanho_array = Number(prompt("Quantas notas você quer para calcular a media?"));
+// contador = 0
+
+// while (contador < tamanho_array){
+//     let valor = Number(prompt("Digite as notas:"));
+//     notas.push(valor);
+//     contador++
+// }
+
+// let media = (notas.reduce((total, nota) => total + nota, 0))/tamanho_array
+
+// alert(`A media das suas notas é ${media}`);
+
+//#######################################################################################
+
+//verificar idade usando ternario###########################################
+//let idade = Number(prompt("fale sua idade"));
+//console.log(`voce é ${idade >= 18 ? 'maior de idade' : 'menor de idade'}`)
+
+//####################################################################################
+
+//vericar numero impar, par, zero
+// let num = Number(prompt("digite um numero"));
+// if(num == 0){
+//     print("numero é zero");
+// } else if(num %2 == 0){
+//      console.log("numero é par");
+// } else{
+//      console.log("numero é impar");
+//     }
+
+//################################################################################
+
+//contar de 1 ate 10
+// let contador = 1
+
+// while(contador <= 10){
+//     console.log(contador);
+//     contador++
+// }
+
+//##########################################################################
+
+//contar de 10 ate 1
+
+// let contador = 10
+
+// while(contador >= 1){
+//     console.log(contador);
+//     contador--
+// }
+
+//###########################################################
+
+//numero aleatorio entre 1 e 1000
+//let num = parseInt(Math.random()* 1001)
+
+//console.log(num)
+
+//##############################################################
+
